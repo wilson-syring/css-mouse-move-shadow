2aad5e487dc740e657362ce611764da925243512
Wilson Syring <wilson.syring@gmail.com>
2023-03-20 12:30:06 -0500

    added my coments for easier following of the JS file

--- js/external.js
+++ js/external.js
@@ -1,19 +1,25 @@
 //variables for selecting the html elements
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+//variable gor the amount of px shadow will move the text shadow
 const walk = 600;
 
 //function for changing the text shadow of element
 function shadow(e) {
-    const { offsetWidth: width, offsetHeight: height } = hero;
-    let { offsetX: x, offsetY: y } = e;
+    //variables for element size
+    const {offsetWidth: width, offsetHeight: height} = hero;
+    //variable for getting cursor position
+    let {offsetX: x, offsetY: y} = e;
+    //if statement for ignoring the children of the parent element for actual cursor location
     if (this !== e.target) {
         x = x + e.target.offsetLeft;
         y = y + e.target.offsetTop;
     }
+    //variables for reducing the walk px by half
     const xWalk = Math.round((x / width * walk) - (walk / 2));
     const yWalk = Math.round((y / height * walk) - (walk / 2));
 
+    //style for the shadows movement and color
     text.style.textShadow = `
     ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
     ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
