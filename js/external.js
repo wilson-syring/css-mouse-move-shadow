9bad0f57a333808806733945c2e5a093f83405c4
Wilson Syring <wilson.syring@gmail.com>
2023-03-20 11:49:44 -0500

    started working on the S file adding element selectors and event listeners

---
+++ js/external.js
@@ -0,0 +1,11 @@
+//variables for selecting the html elements
+const hero = document.querySelector('hero');
+const text = hero.querySelector('h1');
+
+//function for
+function shadow(e){
+const {offsetWidth: width, offsetHeight: height} = hero;
+}
+
+//event listener for mousemove
+hero.addEventListener('mousemove', shadow);
