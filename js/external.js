bfeda48898fdc7e54b66ed61f86e4120813abe22
Wilson Syring <wilson.syring@gmail.com>
2023-03-20 12:19:20 -0500

    completed the guided text shadow exercise

--- js/external.js
+++ js/external.js
@@ -1,10 +1,25 @@
 //variables for selecting the html elements
-const hero = document.querySelector('hero');
+const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const walk = 600;
 
-//function for
-function shadow(e){
-const {offsetWidth: width, offsetHeight: height} = hero;
+//function for changing the text shadow of element
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+    ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+    ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
 }
 
 //event listener for mousemove
